e49431ba660c37454627fb6f23340cb22fe54245
Chimoha31 <mokochii1108@gmail.com>
2022-03-29 10:14:32 -0700

    style for form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div>
-    
+    <div className="formContainer">
+      <form>
+        <h1>Login Form</h1>
+        <hr />
+        <div className="uiForm">
+          <div className="formField">
+            <label>User name</label>
+            <input type="text" placeholder="user name" name="username"></input>
+          </div>
+          <div className="formField">
+            <label>E-mail</label>
+            <input type="text" placeholder="email address" name="mailAddress"></input>
+          </div>
+          <div className="formField">
+            <label>Password</label>
+            <input type="text" placeholder="password" name="password"></input>
+          </div>
+          <button className="submitButton">Login</button>
+        </div>
+      </form>
     </div>
   );
 }
